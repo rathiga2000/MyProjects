5bf76f9f11f752796fca7fe9b3bbb17117ab6865
Rathiga <rathiga.k3@gmail.com>
2023-05-23 21:14:37 +0700

    update

--- SampleProject/src/Hello/Add.java
+++ SampleProject/src/Hello/Add.java
@@ -1,13 +1,22 @@
 package Hello;
+import java.util.Scanner;
 
 public class Add {
 	
-	void Sum()
+	void add(int a, int b)
 	{
-		int a=5;
-		int b=10;
 		System.out.println(a+b);
 		
 	}
-
+public static void main(String arg[])
+{
+	Add obj=new Add();
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the a value");
+	int a=s.nextInt();
+	System.out.println("Enter the b value");
+	int b=s.nextInt();
+	System.out.println("total add value");
+		obj.add(a, b);	
+}
 }
--- SampleProject/src/While/OrderNum.java
+++ SampleProject/src/While/OrderNum.java
@@ -1,10 +1,14 @@
 package While;
-
+import java.util.Scanner;
 public class OrderNum {
 
 	public static void main(String args[]) {
 		OrderNum obj = new OrderNum();
-		obj.Line(1);
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Number Value");
+		int Num=s.nextInt();
+		System.out.println("order of number");
+		obj.Line(Num);
 	}
 
 	public void Line(int Num) {
